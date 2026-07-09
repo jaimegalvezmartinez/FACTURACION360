f5118cf0ce013d739878d5e0bb592fb9b588b2c7
Val <valexx55@gmail.com>
2026-07-09 12:37:01 +0200

    gitignore

---
+++ facturacion360/src/main/java/edu/xtd/facturacion360/Facturacion360Application.java
@@ -0,0 +1,13 @@
+package edu.xtd.facturacion360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Facturacion360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Facturacion360Application.class, args);
+	}
+
+}
---
+++ facturacion360/src/test/java/edu/xtd/facturacion360/Facturacion360ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.xtd.facturacion360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Facturacion360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
